ed7cf71a883f518b305001b2d4900621f20357ad
rowanna <rowankimna@gmail.com>
2025-01-16 09:30:29 +0900

    feat: Context API Modal 이런방법으로 바꾸면 될까

--- src/assets/components/ContextAPIComponent/CustomModalResult.jsx
+++ src/assets/components/ContextAPIComponent/CustomModalResult.jsx
@@ -1,9 +1,14 @@
 import { createContext, useContext, useState } from "react";
 
-const ModalContext = createContext({ show: () => {}, close: () => {} });
+const ModalContext = createContext({
+  show: () => {},
+  close: () => {},
+  title: "",
+});
 
 function ModalContextProvider({ children }) {
   const [open, setOpen] = useState(false);
+  const [title, setTitle] = useState("");
 
   function show() {
     setOpen(true);
@@ -14,25 +19,36 @@ function ModalContextProvider({ children }) {
   }
 
   return (
-    <ModalContext.Provider value={{ show, close }}>
+    <ModalContext.Provider value={{ show, close, title, setTitle }}>
       {children}
       <dialog open={open}>
-        <h3>Modal</h3>
+        <h3>{title}</h3>
         <button onClick={(e) => close()}>닫기</button>
       </dialog>
     </ModalContext.Provider>
   );
 }
 
-function ModalButton() {
-  const { show } = useContext(ModalContext);
-  return <button onClick={(e) => show()}>열기</button>;
+function ModalButton({ children, title }) {
+  const { show, setTitle } = useContext(ModalContext);
+  return (
+    <button
+      onClick={(e) => {
+        show();
+        setTitle(title);
+      }}
+    >
+      {children}
+    </button>
+  );
 }
 
 function CustomModalResult() {
   return (
     <ModalContextProvider>
-      <ModalButton />
+      <ModalButton title="모달1">모달1열기</ModalButton>
+      <ModalButton title="모달2">모달2열기</ModalButton>
+      <ModalButton title="모달3">모달3열기</ModalButton>
     </ModalContextProvider>
   );
 }
